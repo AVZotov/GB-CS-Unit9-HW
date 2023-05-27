7586351a047ba7bec1efdb4b4ffc791c39fe3ce6
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-05-27 20:48:03 +0300

    Exersise001 proj added

---
+++ Exersise001/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int number = ReadInt("Enter positive number: ");
+PrintNumber(number);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    uint number;
+
+    while (!(uint.TryParse(Console.ReadLine(), out number)))
+        Console.Write("Enter positive number to continue: ");
+
+    return (int)number;
+}
+
+void PrintNumber(int number)
+{
+    if (number < 1)
+    {
+        return;
+    }
+        Console.Write(number + " ");
+        PrintNumber(number - 1);
+}
+
+
+
+
