1a8fa866187da72b38bea481e4524a35c35ad16c
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-05-27 22:35:12 +0300

    Exersise003 proj added

---
+++ Exersise003/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int m = ReadInt("Enter first value: ");
+int n = ReadInt("Enter second value: ");
+Console.WriteLine($"Counting Akkerman function result = {AkkFunc(m, n)}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int number;
+
+    while ((int.TryParse(Console.ReadLine(), out number)) == false || number < 0)
+        Console.Write("Enter positive number to continue: ");
+
+    return number;
+}
+
+int AkkFunc(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    
+    else if (n == 0)
+        return AkkFunc(m - 1, 1);
+    else
+        return AkkFunc(m - 1, AkkFunc(m, n - 1));
+}
